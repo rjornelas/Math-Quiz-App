feee8246f60e7cd36d4e35c7effd79c2f4efdd79
rjornelas <rafael.j.ornelas@gmail.com>
2024-01-08 22:14:34 -0300

    Implemented start game from each MainActivity button

--- app/src/main/java/com/rjornelas/math_quiz_app/MainActivity.kt
+++ app/src/main/java/com/rjornelas/math_quiz_app/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.rjornelas.math_quiz_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rjornelas.math_quiz_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnEasy?.setOnClickListener{startGame("easy")}
+        binding?.btnMedium?.setOnClickListener{startGame("medium")}
+        binding?.btnHard?.setOnClickListener{startGame("hard")}
+
+    }
+
+    private fun startGame(questionType: String){
+        val intent = Intent(this, PlayActivity::class.java)
+        intent.putExtra("questionType", questionType)
+        startActivity(intent)
     }
 }

bc3391a3d46817984ae6781e0f4ef9fea1532aa2
rjornelas <rafael.j.ornelas@gmail.com>
2024-01-09 20:10:30 -0300

    Added splash screen

--- app/src/main/java/com/rjornelas/math_quiz_app/MainActivity.kt
+++ app/src/main/java/com/rjornelas/math_quiz_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rjornelas.math_quiz_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rjornelas.math_quiz_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
